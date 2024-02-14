ddb9217ce50ef3caedfe7b9d89ca9c1e52f8c483
ibssoln <itbisys.system@gmail.com>
2024-02-14 16:57:09 -0500

    Adds spring boot

---
+++ src/main/java/com/priceline/chutes/ChutesApplication.java
@@ -0,0 +1,13 @@
+package com.priceline.chutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priceline/chutes/ChutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceline.chutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
