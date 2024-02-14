5e51553cd79e3990c27176728cd34e78eb1724c0
ibssoln <itbisys.system@gmail.com>
2024-02-14 17:50:04 -0500

    Cleanup

--- src/main/java/com/priceline/chutes/Game.java
+++ src/main/java/com/priceline/chutes/Game.java
@@ -5,9 +5,11 @@ import com.priceline.chutes.framework.Controller;
 
 public class Game {
 
+    //************************************************
     //NOTE: You can run a game in 2 different ways.
     // 1. By sending a request to the RESTful service endpoint after the Spring Boot server is up and running.
     // 2. By running a gradle build (gradle run --args="one two three four") in the command line, where the args are the names of the players.
+    //************************************************
 
     //NOTE: To achieve maintainability and extensibility for a later extension of the whole application, the specific 'chutes-and-ladders' related functionalities
     //have been moved to the new class named 'Controller.java'.
--- src/main/java/com/priceline/chutes/GameApplication.java
+++ src/main/java/com/priceline/chutes/GameApplication.java
@@ -3,16 +3,17 @@ package com.priceline.chutes;
 import com.priceline.chutes.conventionalgame.ConventionalGameController;
 import com.priceline.chutes.framework.Controller;
 import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameApplication{ //NOTE: This is another addition to make the entire app as a Spring-Boot supported to increase maintainability & extensibility.
 
+	//************************************************
 	//NOTE: You can run a game in 2 different ways.
 	// 1. By sending a request to the RESTful service endpoint after the Spring Boot server is up and running.
 	// 2. By running a gradle build (gradle run --args="one two three four") in the command line, where the args are the names of the players.
+	//************************************************
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
