7e14c241f596a310a80d881f59f10796a2bc7610
ibssoln <itbisys.system@gmail.com>
2024-02-15 17:38:41 -0500

    Adds easymock

--- src/test/java/service/ChutesLadderGameServiceTest.java
+++ src/test/java/service/ChutesLadderGameServiceTest.java
@@ -2,27 +2,34 @@ package service;
 
 import com.priceline.chutes.entity.chutesladder.Player;
 import com.priceline.chutes.service.ChutesLadderGameService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.easymock.EasyMockRunner;
+import org.easymock.TestSubject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.*;
 
+@RunWith(EasyMockRunner.class)
 public class ChutesLadderGameServiceTest {
+
+    @TestSubject
+    private ChutesLadderGameService service = new ChutesLadderGameService();
 
     private String[] playerNames;
     private List<Player> players;
 
-    @BeforeEach
+    @Before
     public void setupTestVars(){
         playerNames = new String[]{"Jane", "Mike", "Sue", "Ben", "Carlos"};
         players = new ArrayList(List.of(new Player("Jane"), new Player("Mike"), new Player("Sue")));
     }
 
+
     @Test
     public void given4PlayerNames_whenInitiateGame_thenReturnDetermineOrderPlayGameReturnWinner() throws Exception {
-        ChutesLadderGameService service = new ChutesLadderGameService();
         String[] fourPlayers = Arrays.copyOfRange(playerNames, 0, 4);
         String winner = service.initiateGame(fourPlayers);
         assertNotNull(winner);
@@ -30,7 +37,6 @@ public class ChutesLadderGameServiceTest {
 
     @Test
     public void whenSpin_thenReturnNumberBetween1And6BothEndsInclusive(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         int result = service.spin();
         assertTrue(result >= 1);
         assertTrue(result <= 6);
@@ -38,7 +44,6 @@ public class ChutesLadderGameServiceTest {
 
     @Test
     public void given3PlayerNames_whenCreateBasePlayers_thenReturn3Players(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         String[] threePlayers = Arrays.copyOfRange(playerNames, 0, 3);
         List<Player> results = service.createBasePlayers(threePlayers);
         assertNotNull(results);
@@ -47,7 +52,6 @@ public class ChutesLadderGameServiceTest {
 
     @Test
     public void given5PlayerNames_whenCreateBasePlayers_thenReturnEmptyList(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         String[] fivePlayers = Arrays.copyOfRange(playerNames, 0, 5);
         List<Player> results = service.createBasePlayers(fivePlayers);
         assertNotNull(results);
@@ -56,7 +60,6 @@ public class ChutesLadderGameServiceTest {
 
     @Test
     public void givenOnly1PlayerName_whenCreateBasePlayers_thenReturnEmptyList(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         String[] onePlayer = Arrays.copyOfRange(playerNames, 0, 1);
         List<Player> results = service.createBasePlayers(onePlayer);
         assertNotNull(results);
@@ -65,16 +68,13 @@ public class ChutesLadderGameServiceTest {
 
     @Test
     public void givenPlayers_whenDeterminePlayOrder_thenReturnPlayersInDeterminedOrder(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         assertNotNull(service.determinePlayOrder(players));
         assertEquals(3, service.determinePlayOrder(players).size());
     }
 
     @Test
     public void givenPlayers_whenPlayGame_thenReturnWinner(){
-        ChutesLadderGameService service = new ChutesLadderGameService();
         assertNotNull(service.playGame(players));
     }
-
 
 }
